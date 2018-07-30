7438741b11bb7359221aee878f0f813f01d2089e
dimitrov9 <dimitrov333@hotmail.com>
2018-07-30 04:53:38 +0200

    Section 1 - Completed - Added remaining components: * Products * Check-out * Order-Success * Login - moved manage-products and manage-orders components to admin folder - Implemented firebaseOptionsToken fix - fixed the routes and updated to /admin/... for products and orders - in the styles.css I put maring:0 to body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, FirebaseOptionsToken } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { MatComponentsModule } from './mat-components.module';
@@ -16,9 +17,12 @@ import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
-import { ManageOrdersComponent } from './components/manage-orders/manage-orders.component';
-import { ManageProductsComponent } from './components/manage-products/manage-products.component';
-import { environment } from '../environments/environment';
+import { ManageOrdersComponent } from './components/admin/manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './components/admin/manage-products/manage-products.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -30,22 +34,30 @@ import { environment } from '../environments/environment';
     ShoppingCartComponent,
     MyOrdersComponent,
     ManageOrdersComponent,
-    ManageProductsComponent
+    ManageProductsComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule,
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'login', component: LoginComponent },
       { path: 'my-orders', component: MyOrdersComponent },
-      { path: 'manage-orders', component: ManageOrdersComponent },
-      { path: 'manage-products', component: ManageProductsComponent },
+      { path: 'admin/orders', component: ManageOrdersComponent },
+      { path: 'admin/products', component: ManageProductsComponent },
 
 
       { path: '**', component: NotFoundComponent },
@@ -54,7 +66,9 @@ import { environment } from '../environments/environment';
     FlexLayoutModule,
     MatComponentsModule
   ],
-  providers: [],
+  providers: [
+    { provide: FirebaseOptionsToken, useValue: environment.firebase }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
